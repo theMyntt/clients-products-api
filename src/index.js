c34da96b4307a83018c908681cd4700d4552f8bf
Gabriel Araújo Lima <gabriel.araujo@focustextil.com.br>
2024-02-06 10:58:41 -0300

    feat: sign up system

--- src/index.js
+++ src/index.js
@@ -6,22 +6,48 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.post("/clients/create/", async (req, res) => {
+  const postData = {
+    name: req.body.name,
+    email: req.body.email.toLowerCase(),
+    cellphone: req.body.cellphone,
+    birthday: req.body.birthday,
+    address: req.body.address,
+    addressComplement: req.body.addressComplement,
+    city: req.body.city,
+    cep: req.body.cep,
+    createdAt: req.body.createdAt,
+  };
 
-app.get("/clients", async (req, res) => {
+  const check = await ClientCollection.findOne({email: postData.email});
+  if (check) {
+    res.status(400).json({
+      message: "Email já cadastrado",
+    });
+  } else {
+    await ClientCollection.insertMany([postData]);
+    res.status(200).json({
+      message: "Cliente cadastrado com sucesso",
+    });
+  }
+});
+
+app.get("/clients/", async (req, res) => {
   const getData = {
     email: req.query.email.toLowerCase(),
   };
 
-//   if ()
-  const check = await ClientCollection.findOne({email: getData.email});
+  //   if ()
+  const check = await ClientCollection.findOne({ email: getData.email });
   if (check) {
-    res.status(200).json(JSON.stringify(check));
-  }else {
+    res.status(200).send(check);
+  } else {
     res.status(404).json({
       message: "Cliente não encontrado",
     });
   }
 });
+
 
 app.listen(3000, () => {
   console.log("listening on port " + port);
