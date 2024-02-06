90db18d8c48abb7d8433f928e7965dd5988db73e
Gabriel Araújo Lima <gabriel.araujo@focustextil.com.br>
2024-02-06 10:31:08 -0300

    feat: API for recive infos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+const { ClientCollection, ReqProducts } = require("./schema");
+
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.get("/clients", async (req, res) => {
+  const getData = {
+    email: req.query.email.toLowerCase(),
+  };
+
+//   if ()
+  const check = await ClientCollection.findOne({email: getData.email});
+  if (check) {
+    res.status(200).json(JSON.stringify(check));
+  }else {
+    res.status(404).json({
+      message: "Cliente não encontrado",
+    });
+  }
+});
 
 app.listen(3000, () => {
-    console.log("listening on port " + port);
-})
+  console.log("listening on port " + port);
+});
