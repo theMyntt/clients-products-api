020e8ee6f4a90561218353421a7a065231ddf694
Gabriel Araújo Lima <gabriel.araujo@focustextil.com.br>
2024-02-07 08:38:21 -0300

    feat: schema for log collection

--- src/model/model.js
+++ src/model/model.js
@@ -1,5 +1,6 @@
 const clientSchema = require("./schema/clientSchema");
 const requestProductsSchema = require("./schema/requestProductsSchema");
+const logSchema = require("./schema/logSchema");
 
 const mongoose = require("mongoose");
 const mongo_url =
@@ -16,5 +17,6 @@ mongoose
 
 const ClientCollection = mongoose.model("Client", clientSchema);
 const ReqProducts = mongoose.model("ReqProducts", requestProductsSchema);
+const logCollection = mongoose.model("log", logSchema);
 
-module.exports = { ClientCollection, ReqProducts };
+module.exports = { ClientCollection, ReqProducts, logCollection };  
---
+++ src/model/schema/logSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const logSchema = mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = logSchema;
