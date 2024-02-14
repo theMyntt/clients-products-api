e11919ca269b3176262a4e4d3283caf1d8683c72
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-14 13:02:42 -0300

    feat: new sort array method

--- src/utils/dateOrdenate.js
+++ src/utils/dateOrdenate.js
@@ -1,19 +1,5 @@
 function dateOrdenate(data) {
-  let newArr = [];
-  for (let i = 0; i < data.length; i++) {
-    const date = new Date(data[i].createdAt);
-    const dateTime = date.getTime();
-    let arrayPosition = 0;
-    for (let i2 = 0; i2 < data.length; i2++) {
-      let iDate = new Date(data[i2].createdAt);
-      let iDateTime = iDate.getTime();
-      if (dateTime > iDateTime) {
-        arrayPosition++;
-      }
-    }
-    newArr[arrayPosition] = data[i];
-  }
-  return newArr;
+  return data.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)).slice().reverse()
 }
 
 module.exports = dateOrdenate;
